f9d5de71ee10ad1b4b29a1fb40f1d8894d978275
Mehdi <155908312+3ezzy@users.noreply.github.com>
2024-12-01 21:57:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function renderGame() {
         isAlive = false
     }
     messageEl.textContent = message 
-
+}
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
         let card = getRandomCard()
